ee67a42237162cb59a218c7edd4e188ebaca6615
Gregory Carter <gcarter@openaprs.net>
2022-04-02 00:54:48 +0000

    Switch to rows

--- src/DBI.cpp
+++ src/DBI.cpp
@@ -106,7 +106,7 @@ namespace aprsinject {
                                     aprs->timestamp()
                                    );
       TLOG(LogWarn, << "*** MySQL++ Error{Inject::raw}: #"
-                    << res.num_rows()
+                    << res.rows()
                     << std::endl);
       query = _sqlpp->query();
 
